f9b845c32117ae97e82cc735e21309d38f3afcec
unknown <C:\Users\fs\AppData\Roaming\The Bat!>
2015-12-02 02:43:36 +0600

    bunch

--- dist/js/init.js
+++ dist/js/init.js
@@ -15,10 +15,28 @@ $(function() {
 
 
 
-
+	for (var i = 50; i >= 0; i--) {
+		$('.pick-date__list').append($('.pick-date__item').last().clone());
+	};
 
 	$('.pick-date__item').click(function() {
+		var $el = $(this);
 		$('.pick-date__item').removeClass('_active');
-		$(this).addClass('_active');
+		$el.addClass('_active');
+
+        $.ajax({
+            url:"date_time.php",
+            type: "POST",
+            data: {
+            	this_date: $el.data('date'),
+            	this_game: $el.data('id')
+            },
+            cache: false,
+            success: function(responce){ 
+                $('.reserve-form__time').show().html(responce);
+            }
+        });
 	})
+
+
 });
